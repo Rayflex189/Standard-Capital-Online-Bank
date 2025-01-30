bc88b4ecf044ea71234637f57ae02812eda2ccc3
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-01-30 09:46:52 -1100

    Update settings.py
    DONE

--- SCOB/SCOB/settings.py
+++ SCOB/SCOB/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
     # Add more directories if needed
 ]
 
+# Define STATIC_ROOT to collect static files for production and staging
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Directory where Django will collect static files for deployment
 MEDIA_URL = '/external_st/'
 
